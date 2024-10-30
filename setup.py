8074db4a1119161b5cb1fd443d148466708d6f5b
benkimz <benkim3619@gmail.com>
2024-10-30 18:07:38 +0300

    Rename package to shadowserver

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='shadowproxy',
-    version='0.1.0',
-    description='A simple HTTP/HTTPS proxy server in Python',
+    name='shadowserver',
+    version='0.1.1',
+    description='A HTTP/HTTPS proxy server in Python',
     author='Benard K. Wachira',
     packages=find_packages(),
     install_requires=[
